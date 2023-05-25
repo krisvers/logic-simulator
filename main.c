69d731aaf6e3c45df27adb256a48c9c1e90530aa
krisvers <nakoidevn@gmail.com>
2023-05-24 21:40:14 -0400

    Added a bunch of helper functions

--- main.c
+++ main.c
@@ -4,10 +4,6 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-#include "CONFIG.h"
-
-#define RGB(value) (value >> 16) & 0xFF, (value >> 8) & 0xFF, (value & 0xFF)
-
 #define HIGH 1
 #define LOW 0
 
@@ -22,38 +18,36 @@
 #define TYPE_OUTPUT 3
 
 typedef struct {
-	unsigned char type : 2;
+	unsigned char type;
+	bool signal;
 } generic_t;
 
 typedef struct {
-	unsigned char type : 2;
-	bool signal : 1;
-	unsigned char gate : 2;
+	unsigned char type;
+	bool signal;
+	unsigned char gate;
 	void * left;
 	void * right;
 } gate_t;
 
 typedef struct {
-	unsigned char type : 2;
-	bool signal : 1;
+	unsigned char type;
+	bool signal;
 } constant_t;
 
 typedef struct {
-	unsigned char type : 2;
-	bool signal : 1;
+	unsigned char type;
+	bool signal;
 	void * left;
-	void * right;
 } input_t;
 
 typedef struct {
-	unsigned char type : 2;
-	bool signal : 1;
+	unsigned char type;
+	bool signal;
 	void * left;
-	void * right;
 } output_t;
 
 typedef struct {
-	unsigned char type : 2;
 	size_t num_inputs;
 	input_t ** inputs;
 	size_t num_outputs;
@@ -76,9 +70,11 @@ bool evaluate(generic_t * node) {
 				lvalue = evaluate(left);
 			}
 
+			node->signal = lvalue;
 			return lvalue;
 		case TYPE_CONSTANT:
-			return ((constant_t *) node)->signal;
+			node->signal = ((constant_t *) node)->signal;
+			return node->signal;
 		case TYPE_GATE:
 			left = (generic_t *) ((gate_t *) node)->left;
 			right = (generic_t *) ((gate_t *) node)->right;
@@ -96,13 +92,17 @@ bool evaluate(generic_t * node) {
 
 			switch (((gate_t *) node)->gate) {
 				case GATE_NOT:
-					return (lvalue == HIGH) ? LOW : HIGH;
+					node->signal = (lvalue == HIGH) ? LOW : HIGH;
+					return node->signal;
 				case GATE_AND:
-					return (lvalue == HIGH && rvalue == HIGH) ? HIGH : LOW;
+					node->signal = (lvalue == HIGH && rvalue == HIGH) ? HIGH : LOW;
+					return node->signal;
 				case GATE_OR:
-					return (lvalue == HIGH || rvalue == HIGH) ? HIGH : LOW;
+					node->signal = (lvalue == HIGH || rvalue == HIGH) ? HIGH : LOW;
+					return node->signal;
 				case GATE_XOR:
-					return ((lvalue == HIGH && rvalue == LOW) || (rvalue == HIGH && lvalue == LOW)) ? HIGH : LOW;
+					node->signal = ((lvalue == HIGH && rvalue == LOW) || (rvalue == HIGH && lvalue == LOW)) ? HIGH : LOW;
+					return node->signal;
 				default:
 					fprintf(stderr, "evaluate(): invalid gate type %u\n", ((gate_t *) node)->gate);
 					abort();
@@ -116,8 +116,9 @@ bool evaluate(generic_t * node) {
 				lvalue = evaluate(left);
 			}
 
+			node->signal = lvalue;
 			return lvalue;
-		default: return LOW;
+		default: node->signal = LOW; return LOW;
 	}
 }
 
@@ -136,6 +137,175 @@ bool * evaluate_circuit(circuit_t * circuit) {
 	return outputs;
 }
 
+circuit_t * circuit_new(size_t inputs, size_t outputs) {
+	circuit_t * c = malloc(sizeof(circuit_t));
+	if (c == NULL) {
+		fprintf(stderr, "error: circuit_new(): malloc(sizeof(circuit_t)) failed\n");
+		abort();
+	}
+
+	c->num_inputs = inputs;
+	c->inputs = malloc(sizeof(input_t *) * inputs);
+	c->num_outputs = outputs;
+	c->outputs = malloc(sizeof(output_t *) * outputs);
+
+	return c;
+}
+
+input_t * input_new() {
+	input_t * i = malloc(sizeof(input_t));
+	if (i == NULL) {
+		fprintf(stderr, "error: input_new(): malloc(sizeof(input_t)) failed\n");
+		abort();
+	}
+
+	i->type = TYPE_INPUT;
+	i->left = NULL;
+	i->signal = LOW;
+
+	return i;
+}
+
+gate_t * gate_new(unsigned char type) {
+	gate_t * gate = malloc(sizeof(gate_t));
+	if (gate == NULL) {
+		fprintf(stderr, "error: gate_new(): malloc(sizeof(gate_t)) failed\n");
+		abort();
+	}
+
+	gate->type = TYPE_GATE;
+	gate->gate = type;
+	gate->left = NULL; gate->right = NULL;
+	gate->signal = LOW;
+
+	return gate;
+}
+
+constant_t * constant_new(bool signal) {
+	constant_t * c = malloc(sizeof(constant_t));
+	if (c == NULL) {
+		fprintf(stderr, "error: constant_new(): malloc(sizeof(constant_t)) failed\n");
+		abort();
+	}
+
+	c->type = TYPE_CONSTANT;
+	c->signal = signal;
+
+	return c;
+}
+
+void link_to_input(input_t * i, generic_t * g) {
+	switch (g->type) {
+		case TYPE_INPUT:
+		case TYPE_GATE:
+		case TYPE_CONSTANT:
+		case TYPE_OUTPUT:
+			i->left = (void *) g;
+			break;
+		default:
+			fprintf(stderr, "error: link_to_input(): can't link type %llu with input %p\n", (long long unsigned int) g->type, (void *) i);
+			abort();
+	}
+}
+
+void link_to_gate(gate_t * gate, generic_t * g) {
+	switch (g->type) {
+		case TYPE_GATE:
+			if (gate->gate == GATE_NOT) {
+				if (gate->left != NULL) {
+					fprintf(stderr, "error: link_to_gate(): left node is already linked on the not gate %p\n", (void *) gate);
+					abort();
+				}
+
+				gate->left = (void *) g;
+				break;
+			}
+
+			if (gate->left != NULL) {
+				if (gate->right != NULL) {
+					fprintf(stderr, "error: link_to_gate(): all nodes already linked on gate %p\n", (void *) gate);
+					abort();
+				}
+
+				gate->right = (void *) g;
+				break;
+			}
+
+			gate->left = (void *) g;
+			break;
+		case TYPE_INPUT:
+		case TYPE_CONSTANT:
+		case TYPE_OUTPUT:
+			if (gate->left != NULL) {
+				if (gate->right != NULL) {
+					fprintf(stderr, "error: link_to_gate(): all nodes are already linked on the gate %p\n", (void *) gate);
+					abort();
+				}
+
+				gate->right = (void *) g;
+				break;
+			}
+
+			gate->left = (void *) g;
+			break;
+		default:
+			fprintf(stderr, "error: link_to_gate(): can't link unknown type %llu with gate %p\n", (long long unsigned int) g->type, (void *) g);
+			abort();
+	}
+}
+
+void link_to_output(output_t * o, generic_t * g) {
+	switch (g->type) {
+		case TYPE_INPUT:
+		case TYPE_GATE:
+		case TYPE_CONSTANT:
+		case TYPE_OUTPUT:
+			if (o->left != NULL) {
+				fprintf(stderr, "error: link_to_output(): left node is already linked on the circuit output %p\n", (void *) o);
+				abort();
+			}
+
+			o->left = (void *) g;
+			break;
+		default:
+			fprintf(stderr, "error: link_to_output(): can't link type %llu with circuit output %p\n", (long long unsigned int) g->type, (void *) o);
+			abort();
+	}
+}
+
+void link_to_circuit(circuit_t * c, size_t index, generic_t * g) {
+	if (index >= c->num_outputs) {
+		fprintf(stderr, "error: link_to_circuit(): invalid index to link generic %p to output of circuit %p\n", (void *) g, (void *) c);
+		abort();
+	}
+
+	c->outputs[index] = malloc(sizeof(input_t));
+
+	link_to_output(c->outputs[index], g);
+}
+
 int main() {
+	circuit_t * circuit = circuit_new(2, 2);
+	gate_t * xor = gate_new(GATE_XOR);
+	gate_t * and = gate_new(GATE_AND);
+	input_t * ia = input_new();
+	input_t * ib = input_new();
+	constant_t * a = constant_new(1);
+	constant_t * b = constant_new(1);
+
+	link_to_circuit(circuit, 0, (generic_t *) xor);
+	link_to_circuit(circuit, 1, (generic_t *) and);
+
+	link_to_gate(xor, (generic_t *) ia);
+	link_to_gate(xor, (generic_t *) ib);
+
+	link_to_gate(and, (generic_t *) ia);
+	link_to_gate(and, (generic_t *) ib);
+
+	link_to_input(ia, (generic_t *) a);
+	link_to_input(ib, (generic_t *) b);
+
+	printf("%u, %u\n", evaluate((generic_t *) circuit->outputs[0]), evaluate((generic_t *) circuit->outputs[1]));
+
 	return 0;
 }
